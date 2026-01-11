df52e8b746a023c991f4403760cfe0706649130e
Suvam Ray <suvamray2008@gmail.com>
2026-01-11 07:26:36 +0530

    Mini-project : Deployment using gitHub actions + CodeDeploy for EC2 + ELB + ASG setup

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-# Login to my AWS ECR
-aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 992382647488.dkr.ecr.ap-southeast-2.amazonaws.com
-docker pull 992382647488.dkr.ecr.ap-southeast-2.amazonaws.com/campusx_ecr:v3
-docker stop my-container || true
-docker rm my-container || true
-docker run -d -p 80:5000 -e DAGSHUB_PAT=e691c7193ab61dc9678e31c6b92ded8a65f80697 --name campusx-app 992382647488.dkr.ecr.ap-southeast-2.amazonaws.com/campusx_ecr:v3
+# Login to AWS ECR
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 051826734860.dkr.ecr.ap-southeast-2.amazonaws.com
+
+# Pull the latest image
+docker pull 051826734860.dkr.ecr.ap-southeast-2.amazonaws.com/campusx_ecr:latest
+
+# Check if the container 'campusx-app' is running
+if [ "$(docker ps -q -f name=campusx-app)" ]; then
+    # Stop the running container
+    docker stop campusx-app
+fi
+
+# Check if the container 'campusx-app' exists (stopped or running)
+if [ "$(docker ps -aq -f name=campusx-app)" ]; then
+    # Remove the container if it exists
+    docker rm campusx-app
+fi
+
+# Run a new container
+docker run -d -p 80:5000 -e DAGSHUB_PAT=e691c7193ab61dc9678e31c6b92ded8a65f80697 --name campusx-app 051826734860.dkr.ecr.ap-southeast-2.amazonaws.com/campusx_ecr:latest
